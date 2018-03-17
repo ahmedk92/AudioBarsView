f0f880de028ebc194b0adac6e60e7c00679e7488
Ahmed Khalaf <admin@ahmeds-macbook-pro.local>
2018-03-17 23:02:33 +0200

    demo gif

--- AudioBars/ViewController.swift
+++ AudioBars/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
 
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
